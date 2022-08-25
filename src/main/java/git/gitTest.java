51a4d993c2de9ec06f6aa38055d44961ef136edd
chenrenjie <chenrenjie66@163.com>
2022-08-25 20:56:27 +0800

    1

--- src/main/java/git/gitTest.java
+++ src/main/java/git/gitTest.java
@@ -4,6 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("develop test");
         System.out.println("git test");
-        System.out.println("test");
+        System.out.println("develop");
     }
 }
