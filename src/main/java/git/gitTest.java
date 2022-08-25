7a506ede4ed3a696a3f4c04e46e7d6692c2b0248
chenrenjie <chenrenjie66@163.com>
2022-08-25 20:54:22 +0800

    1

--- src/main/java/git/gitTest.java
+++ src/main/java/git/gitTest.java
@@ -2,15 +2,6 @@ package git;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
-        System.out.println("pull test");
         System.out.println("develop test");
-System.out.println("my test");
     }
 }
